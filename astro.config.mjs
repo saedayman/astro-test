1f8b3d5f4e8fe4ed792496deba1fdd6eb6ba7ba5
sayman <sa467563@gmail.com>
2026-01-21 00:12:59 +0300

    adding astro netlify adpater and fixing build ssg and converting to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid'
+  output: 'server',
+  adapter: netlify()
 });
